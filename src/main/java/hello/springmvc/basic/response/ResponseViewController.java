c35691f3e0a02f7254abf289b859ac16d9ce2697
Son-Gyeongi <thsruddl77@gmail.com>
2023-06-17 20:48:03 +0900

    스프링 MVC 기본 기능 - HTTP응답 - 뷰 템플릿을 호출하는 컨트롤러 (@ResponseBody 조심)

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.response;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,6 +18,7 @@ public class ResponseViewController {
     }
 
     // String으로 반환
+//    @ResponseBody를 쓰면 뷰로 가지않고 문자로 반환된다. 문자가 그대로 HTTP응답 코드로 나간다.
     @RequestMapping("/response-view-v2")
     public String responseViewV2(Model model) {
         model.addAttribute("data", "hello!");
